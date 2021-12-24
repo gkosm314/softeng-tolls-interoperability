3c9dd95d2364b11a4eeb896022c7934b095eb7bf
gkosm314 <gkosm314@gmail.com>
2021-12-24 03:46:05 +0200

    Changed DATABASE settings in settings.py

--- tolls/settings.py
+++ tolls/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'tolls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'tolls_app_database',
+        'USER': 'tolls_root',
+        'PASSWORD': 'tolls1234',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
