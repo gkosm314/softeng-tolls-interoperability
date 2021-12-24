e77d242463f1c490afeb0e9262f496369fdaec59
gkosm314 <gkosm314@gmail.com>
2021-12-24 05:14:02 +0200

    Uncomment admin/hardreset endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('admin/resetpasses', views.admin_resetpasses, name='resetpasses_url'),
     # path('admin/resetstations', views.admin_resetstations, name='resetstations_url'),
     # path('admin/resetvehicles', views.admin_resetvehicles, name='resetvehicles_url'),
-    # path('admin/hardreset', views.admin_hardreset, name='hardreset_url'),
+    path('admin/hardreset', views.admin_hardreset, name='hardreset_url'),
 
     # path('login', views.obtain_auth_token, name = "login_url"), #this refers to the views imported from rest_framework.authtoken
     # path('logout', views.logout_view, name = "logout_url"),
