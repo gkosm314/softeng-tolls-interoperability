4ba6ebdd4799c1efb3d6fd293fac14a033ac6368
gkosm314 <gkosm314@gmail.com>
2021-12-24 04:02:31 +0200

    Removed plural from models' names

--- backend/models.py
+++ backend/models.py
@@ -8,13 +8,13 @@
 from django.db import models
 
 
-class Passes(models.Model):
+class Pass(models.Model):
     passid = models.CharField(db_column='passID', primary_key=True, max_length=10)  # Field name made lowercase.
     timestamp = models.DateTimeField()
-    stationref = models.ForeignKey('Stations', models.DO_NOTHING, db_column='stationRef')  # Field name made lowercase.
-    vehicleref = models.ForeignKey('Vehicles', models.DO_NOTHING, db_column='vehicleRef')  # Field name made lowercase.
+    stationref = models.ForeignKey('Station', models.DO_NOTHING, db_column='stationRef')  # Field name made lowercase.
+    vehicleref = models.ForeignKey('Vehicle', models.DO_NOTHING, db_column='vehicleRef')  # Field name made lowercase.
     charge = models.FloatField()
-    providerabbr = models.ForeignKey('Providers', models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
+    providerabbr = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
     ishome = models.IntegerField(db_column='isHome')  # Field name made lowercase.
 
     class Meta:
@@ -22,10 +22,10 @@ class Passes(models.Model):
         db_table = 'Passes'
 
 
-class Payments(models.Model):
+class Payment(models.Model):
     transactionid = models.AutoField(db_column='transactionID', primary_key=True)  # Field name made lowercase.
-    providercreditedid = models.ForeignKey('Providers', models.DO_NOTHING, db_column='providerCreditedID')  # Field name made lowercase.
-    providerdebitedid = models.ForeignKey('Providers', models.DO_NOTHING, db_column='providerDebitedID')  # Field name made lowercase.
+    providercreditedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerCreditedID')  # Field name made lowercase.
+    providerdebitedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerDebitedID')  # Field name made lowercase.
     amount = models.IntegerField()
     status = models.CharField(max_length=10)
 
@@ -34,7 +34,7 @@ class Payments(models.Model):
         db_table = 'Payments'
 
 
-class Providers(models.Model):
+class Provider(models.Model):
     providerid = models.AutoField(db_column='providerID', primary_key=True)  # Field name made lowercase.
     providername = models.CharField(db_column='providerName', unique=True, max_length=20)  # Field name made lowercase.
     providerabbr = models.CharField(db_column='providerAbbr', unique=True, max_length=2)  # Field name made lowercase.
@@ -45,9 +45,9 @@ class Providers(models.Model):
         db_table = 'Providers'
 
 
-class Stations(models.Model):
+class Station(models.Model):
     stationid = models.CharField(db_column='stationID', primary_key=True, max_length=10)  # Field name made lowercase.
-    stationprovider = models.ForeignKey(Providers, models.DO_NOTHING, db_column='stationProvider')  # Field name made lowercase.
+    stationprovider = models.ForeignKey(Provider, models.DO_NOTHING, db_column='stationProvider')  # Field name made lowercase.
     stationname = models.CharField(db_column='stationName', unique=True, max_length=30)  # Field name made lowercase.
     isvalid = models.IntegerField(db_column='isValid')  # Field name made lowercase.
 
@@ -56,11 +56,11 @@ class Stations(models.Model):
         db_table = 'Stations'
 
 
-class Vehicles(models.Model):
+class Vehicle(models.Model):
     vehicleid = models.CharField(db_column='vehicleID', primary_key=True, max_length=12)  # Field name made lowercase.
     tagid = models.CharField(db_column='tagID', max_length=9)  # Field name made lowercase.
     tagprovider = models.CharField(db_column='tagProvider', max_length=20)  # Field name made lowercase.
-    providerabbr = models.ForeignKey(Providers, models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
+    providerabbr = models.ForeignKey(Provider, models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
     licenseyear = models.IntegerField(db_column='licenseYear')  # Field name made lowercase.
     isvalid = models.IntegerField(db_column='isValid')  # Field name made lowercase.
 
