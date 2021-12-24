3a85f391d61d5430a6b2abad475054525bc1287d
gkosm314 <gkosm314@gmail.com>
2021-12-24 04:05:02 +0200

    Remove autogenerated bug

--- backend/models.py
+++ backend/models.py
@@ -9,22 +9,22 @@ from django.db import models
 
 
 class Pass(models.Model):
-    passid = models.CharField(db_column='passID', primary_key=True, max_length=10)  # Field name made lowercase.
+    passid = models.CharField(db_column='passID', primary_key=True, max_length=10)
     timestamp = models.DateTimeField()
-    stationref = models.ForeignKey('Station', models.DO_NOTHING, db_column='stationRef')  # Field name made lowercase.
-    vehicleref = models.ForeignKey('Vehicle', models.DO_NOTHING, db_column='vehicleRef')  # Field name made lowercase.
+    stationref = models.ForeignKey('Station', models.DO_NOTHING, db_column='stationRef')
+    vehicleref = models.ForeignKey('Vehicle', models.DO_NOTHING, db_column='vehicleRef')
     charge = models.FloatField()
-    providerabbr = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
-    ishome = models.IntegerField(db_column='isHome')  # Field name made lowercase.
+    providerabbr = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerAbbr')
+    ishome = models.IntegerField(db_column='isHome')
 
     class Meta:
         db_table = 'Passes'
 
 
 class Payment(models.Model):
-    transactionid = models.AutoField(db_column='transactionID', primary_key=True)  # Field name made lowercase.
-    providercreditedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerCreditedID', related_name = 'provider_credited_id')  # Field name made lowercase.  
-    providerdebitedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerDebitedID', related_name = 'provider_debited_id')  # Field name made lowercase.  
+    transactionid = models.AutoField(db_column='transactionID', primary_key=True)
+    providercreditedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerCreditedID', related_name = 'provider_credited_id')  
+    providerdebitedid = models.ForeignKey('Provider', models.DO_NOTHING, db_column='providerDebitedID', related_name = 'provider_debited_id')  
     amount = models.IntegerField()
     status = models.CharField(max_length=10)
 
@@ -33,32 +33,32 @@ class Payment(models.Model):
 
 
 class Provider(models.Model):
-    providerid = models.AutoField(db_column='providerID', primary_key=True)  # Field name made lowercase.
-    providername = models.CharField(db_column='providerName', unique=True, max_length=20)  # Field name made lowercase.
-    providerabbr = models.CharField(db_column='providerAbbr', unique=True, max_length=2)  # Field name made lowercase.
-    isvalid = models.IntegerField(db_column='isValid')  # Field name made lowercase.
+    providerid = models.AutoField(db_column='providerID', primary_key=True)
+    providername = models.CharField(db_column='providerName', unique=True, max_length=20)
+    providerabbr = models.CharField(db_column='providerAbbr', unique=True, max_length=2)
+    isvalid = models.IntegerField(db_column='isValid')
 
     class Meta:
         db_table = 'Providers'
 
 
 class Station(models.Model):
-    stationid = models.CharField(db_column='stationID', primary_key=True, max_length=10)  # Field name made lowercase.
-    stationprovider = models.ForeignKey(Provider, models.DO_NOTHING, db_column='stationProvider')  # Field name made lowercase.
-    stationname = models.CharField(db_column='stationName', unique=True, max_length=30)  # Field name made lowercase.
-    isvalid = models.IntegerField(db_column='isValid')  # Field name made lowercase.
+    stationid = models.CharField(db_column='stationID', primary_key=True, max_length=10)
+    stationprovider = models.ForeignKey(Provider, models.DO_NOTHING, db_column='stationProvider')
+    stationname = models.CharField(db_column='stationName', unique=True, max_length=30)
+    isvalid = models.IntegerField(db_column='isValid')
 
     class Meta:
         db_table = 'Stations'
 
 
 class Vehicle(models.Model):
-    vehicleid = models.CharField(db_column='vehicleID', primary_key=True, max_length=12)  # Field name made lowercase.
-    tagid = models.CharField(db_column='tagID', max_length=9)  # Field name made lowercase.
-    tagprovider = models.CharField(db_column='tagProvider', max_length=20)  # Field name made lowercase.
-    providerabbr = models.ForeignKey(Provider, models.DO_NOTHING, db_column='providerAbbr')  # Field name made lowercase.
-    licenseyear = models.IntegerField(db_column='licenseYear')  # Field name made lowercase.
-    isvalid = models.IntegerField(db_column='isValid')  # Field name made lowercase.
+    vehicleid = models.CharField(db_column='vehicleID', primary_key=True, max_length=12)
+    tagid = models.CharField(db_column='tagID', max_length=9)
+    tagprovider = models.CharField(db_column='tagProvider', max_length=20)
+    providerabbr = models.ForeignKey(Provider, models.DO_NOTHING, db_column='providerAbbr')
+    licenseyear = models.IntegerField(db_column='licenseYear')
+    isvalid = models.IntegerField(db_column='isValid')
 
     class Meta:
         db_table = 'Vehicles'
