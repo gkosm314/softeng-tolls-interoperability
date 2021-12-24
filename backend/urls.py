0755768c377986bfd11ca12516d228726b098713
gkosm314 <gkosm314@gmail.com>
2021-12-24 04:59:57 +0200

    Uncomment admin/resetpasses endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     # path('admin/healthcheck', views.admin_healthcheck, name='healthcheck_url'),
     # path('admin/resetpasses', views.admin_resetpasses, name='resetpasses_url'),
-    # path('admin/resetstations', views.admin_resetstations, name='resetstations_url'),
+    path('admin/resetstations', views.admin_resetstations, name='resetstations_url'),
     # path('admin/resetvehicles', views.admin_resetvehicles, name='resetvehicles_url'),
     # path('admin/hardreset', views.admin_hardreset, name='hardreset_url'),
 
