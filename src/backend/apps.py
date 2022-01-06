d952ce3af036c092fe525662e995b6670af0c301
Apostolis <apostolis.stamatis1@gmail.com>
2022-01-07 01:47:06 +0200

    adding signal to auto create group on provider instance creation

--- src/backend/apps.py
+++ src/backend/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend'
+
+    # Register signals here
+    def ready(self):
+        # Implicitly connect a signal handlers decorated with @receiver.
+        from . import signals
