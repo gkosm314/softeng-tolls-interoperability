a70d6727b2d5d12583f0dd384600046138806434
gkosm314 <gkosm314@gmail.com>
2022-01-12 19:19:28 +0200

    Renames api views so that they are different from the relevant backend implementations

--- src/api/urls.py
+++ src/api/urls.py
@@ -6,18 +6,18 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 #TODO: handle csv format admin/healthcheck?format=<response_format>'
 
 urlpatterns = [
-    path('admin/healthcheck', views.admin_healthcheck, name='healthcheck_url'),
-    path('admin/resetpasses', views.admin_resetpasses, name='resetpasses_url'),
-    path('admin/resetstations', views.admin_resetstations, name='resetstations_url'),
-    path('admin/resetvehicles', views.admin_resetvehicles, name='resetvehicles_url'),
-    path('admin/hardreset', views.admin_hardreset, name='hardreset_url'),
+    path('admin/healthcheck', views.api_admin_healthcheck, name='healthcheck_url'),
+    path('admin/resetpasses', views.api_admin_resetpasses, name='resetpasses_url'),
+    path('admin/resetstations', views.api_admin_resetstations, name='resetstations_url'),
+    path('admin/resetvehicles', views.api_admin_resetvehicles, name='resetvehicles_url'),
+    path('admin/hardreset', views.api_admin_hardreset, name='hardreset_url'),
 
-    path('login', views.obtain_auth_token, name="login_url"), #this refers to the views imported from rest_framework.authtoken
+    path('login', views.LoginView.as_view(), name="login_url"),
     path('logout', views.logout_view, name="logout_url"),
-    path('PassesPerStation/<stationID>/<datefrom>/<dateto>', views.PassesPerStation.as_view(), name='passes_per_station'),
-    path('PassesAnalysis/<op1_ID>/<op2_ID>/<datefrom>/<dateto>', views.PassesAnalysis.as_view(), name='passes_analysis'),
-    path('PassesCost/<op1_ID>/<op2_ID>/<datefrom>/<dateto>', views.PassesCost.as_view(), name='passes_analysis'),
-    path('ChargesBy/<op_ID>/<datefrom>/<dateto>', views.ChargesBy.as_view(), name='passes_analysis'),
+    path('PassesPerStation/<stationID>/<datefrom>/<dateto>', views.ApiPassesPerStation.as_view(), name='passes_per_station'),
+    path('PassesAnalysis/<op1_ID>/<op2_ID>/<datefrom>/<dateto>', views.ApiPassesAnalysis.as_view(), name='passes_analysis'),
+    path('PassesCost/<op1_ID>/<op2_ID>/<datefrom>/<dateto>', views.ApiPassesCost.as_view(), name='passes_analysis'),
+    path('ChargesBy/<op_ID>/<datefrom>/<dateto>', views.ApiChargesBy.as_view(), name='passes_analysis'),
 
     # Schema urls
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
