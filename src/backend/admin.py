3926fe9ea28584bbee0d0b7ac30497cc7e2e45f3
Apostolis <apostolis.stamatis1@gmail.com>
2022-02-11 17:49:34 +0200

    Deleting src folder

--- src/backend/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Pass, Payment, Provider, Station, Vehicle
-
-# Register your models here.
-admin.site.register(Pass)
-admin.site.register(Payment)
-admin.site.register(Provider)
-admin.site.register(Station)
-admin.site.register(Vehicle)
